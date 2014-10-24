308d09c39d5ff923f5f77f9e27ac3d0fe2e8f7c3
Niklas Wikstrom <nikwik@foi.se>
2014-10-24 14:06:19 +0200

    Adding functionality to structuralModes writePatchPointLabels

--- src/oceanWaves/testFunctions.py
+++ src/oceanWaves/testFunctions.py
@@ -24,35 +24,36 @@ def elevation(wh,wl,wd,t):
     c = celerity(wl,wd,g)
     w = omega(c,k)
 
-    h = A*cos(w*t)                                \
-        + sqr(A)*k*cosh(kh)/(4*pow(sinh(2*kh),3)) \
+    h = A*cos(w*t)                              \
+        + sqr(A)*k*cosh(kh)/(4*pow(sinh(kh),3)) \
         * (2+cosh(2*kh))*cos(2*(w*t))
 
-    h1= A*sin(w*t)                                \
-        + sqr(A)*k*sinh(kh)/(4*pow(cosh(2*kh),3)) \
-        * (2+sinh(2*kh))*sin(2*(w*t))
+    h1 = A*sin(w*t)                              \
+        + sqr(A)*k*cosh(kh)/(4*pow(sinh(kh),3)) \
+        * (2+cosh(2*kh))*sin(2*(w*t))
 
     sigma = tanh(kh)
-    h2= A*(sin(w*t)+k*A*(3-sigma**2)/(4*sigma**3)*sin(2*w*t))
+    h2= A*(cos(w*t)+k*A*(3-sigma**2)/(4*sigma**3)*cos(2*w*t))
 
 
     return h,h1,h2
 
 
 wh = 0.2
-wl = 2.0
+wl = 1.5
 wd = 2.2
 g  = 9.81
 
 c = celerity(wl,wd,g)
 k = waveNumber(wl)
 print 'celerity = {0}\nperiod = {1}'.format(c, period(c,k))
+print 'k*a =a*2pi/L = {0}'.format(wh/2*2*pi/wl)
 
-T = arange(0,pi/2,0.01)
+T = arange(0,period(c,k),0.01)
 
 h,h1,h2 = elevation(wh,wl,wd,T)
 
-plot(T,h)
-plot(T,h1)
-plot(T,h2)
+plot(T,h,'--')
+plot(T,h1,'-')
+#plot(T,h2,'.')
 show()
