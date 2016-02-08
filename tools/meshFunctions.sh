cfc9005e5008125634922399712a4ffaacefcae1
Niklas Wikstrom <nikwik@foi.se>
2016-02-08 22:09:29 +0100

    Updating meshFunctions.sh

--- tools/meshFunctions.sh
+++ tools/meshFunctions.sh
@@ -4,17 +4,44 @@ function versionIsNewerThan()
     test $(echo -e "$1\n$WM_PROJECT_VERSION" | sort -V|head -1) != $1                    
     echo $?                                                                     
 }                                                                               
-  
+
+#- Just a wrapper that informs and pipes stdout to log.
 runme()
 {
     echo -e "Running: $@"
     [[ -d logs ]] || mkdir logs
-    "$@" > logs/$1.log
+    [[ "$1" == "mpirun" ]] &&  log=$4 || log=$1
+    "$@" > logs/$log.log 2>&1
     if [ $? != "0" ]; then
-        cat logs/$1.log
+        cat logs/$log.log
         echo -e "Some error! Exiting."
         exit 1
     fi
+}
+
+#- Wrapper around runme, selecting serial or parallel run
+runmePar()
+{
+    np=$1
+    shift 1
+    if [ "$np" == "1" ]; then
+        runme "$@"
+    else
+        runme mpirun -np $np "$@" -parallel
+    fi
+}
+
+
+setDecomposition()
+{
+    dict="system/decomposeParDict"
+    if [ "$1" == "1" ]; then
+        return 0
+    elif [ ! -f $dict ]; then
+        echo "Error: Missing decomposeParDict"
+        exit 1
+    fi
+    sed -i -e "s%^\s*\(numberOfSubdomains\) .*;%\1 $1;%g" $dict
 }
 
 newTempCase()
@@ -24,6 +51,24 @@ newTempCase()
     ( touch $1/case.foam )
     runme cp -r setup.foam constant system $1/.
 }
+
+#- Replaces processor*/0 with serial 0, to keep templates
+distributeZero()
+{
+    [[ -d processor0 ]] || return 0
+
+    if [[ "$1" == "" ]]; then
+        sourceDir=0
+    else
+        sourceDir=$1
+    fi
+
+    for p in processor*; do
+        rm -rf $p/0
+        cp -rv 0 $p/0
+    done
+}
+
 
 
 extrude()
